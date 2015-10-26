e309a1ee1f9ef98fed85823494fa4be5f6787118
Jennifer Hung <jennifersukay505@gmail.com>
2015-10-26 14:21:22 -0600

    began executive statement

---
+++ public_html/documentation/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html.
+	<html lang="en">
+	<head>
+	<title>Executive Summary</title>
+</head>
+<body>
+	<h1>Executive Summary</h1>
+	<blockquote>"Depolarizing Society"<blockquote>
+		<P>
+</body>
