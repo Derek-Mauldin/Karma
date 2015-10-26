e1e2603871c26b275f06fdcba903547bfae4a12c
Derek Mauildin <derek.e.mauldin@gmail.com>
2015-10-26 14:34:36 -0600

    added .gitignore, composer.json, directories public_html and documentation, entity-relationship.php, index.php, and use-cases.php

--- public_html/documentation/index.php
+++ public_html/documentation/index.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 	<html lang="en">
-	<head>
-	<title>Executive Summary</title>
-</head>
-<body>
-	<h1>Executive Summary</h1>
-	<blockquote>"Depolarizing Society"<blockquote>
-		<P>
-</body>
+		<head>
+			<meta charset="UTF-8" />
+			<title>Executive Summary</title>
+		</head>
+
+		<body>
+			<h1>Executive Summary</h1>
+			<blockquote>"Depolarizing Society"<blockquote>
+		</body>
+
+	</html>
