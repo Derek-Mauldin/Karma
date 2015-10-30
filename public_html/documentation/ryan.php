c3780dcde6e83c9f5d5eb235fc371e49986d611f
Jennifer Hung <jennifersukay505@gmail.com>
2015-10-30 12:48:26 -0600

    finished ryan.php

---
+++ public_html/documentation/ryan.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Ryan Fisher</title>
+	</head>
+	<body>
+		<article>
+			<section>
+				<h1>Persona 2</h1>
+				<table>
+					<tr>
+						<th>Name</th>
+						<td>Ryan Fisher</td>
+					</tr>
+					<tr>
+						<th>Age</th>
+						<td>22 years old</td>
+					</tr>
+					<tr>
+						<th>Location</th>
+						<td>Albuquerque</td>
+					</tr>
+					<tr>
+						<th>Profession</th>
+						<td>Starbucks Employee</td>
+					</tr>
+					<tr>
+						<th>Hobby</th>
+						<td>Soccer Club(passion)</td>
+					</tr>
+					<tr>
+						<th>Tech Level</th>
+						<td>High</td>
+					</tr>
+					<tr>
+						<th>Background</th>
+						<td>Attending college in Albuquerque, NM.  New in town. Found out about karma.com while looking for volunteer opportunities on the web.</td>
+					</tr>
+				</table>
+			</section>
+			<section>
+				<h2>Use Case</h2>
+				<ul>
+					<li>After accessing the website and noting the premise, he wanted to take a closer look</li>
+					<li>Ryan found he could not access user profiles without signing up.</li>
+					<li>Ryan clicked “join” and filled out his profile information to sign up.</li>
+					<li>He added “soccer coaching” to “”gift” and “a home cooked meal” to his profile “need”.</li>
+					<li>He received a confirmation email immediately, and clicked back onto the site.</li>
+					<li>Ryan perused the profiles and enjoyed seeing like-minded individuals on the site wanting to give back to society.</li>
+					<li>Ryan bookmarked karma.com</li>
+					<li>Ryan received an alert saying he had messages waiting at karma.com</li>
+					<li>In his mailbox, Ryan found two messages.  One, from a 18-year- boy named Samuel.  The other email was  from a 34-year-old woman named Pearl.   Both offered Ryan a home cooked meal  Ryan was happy.</li>
+					<li>Ryan accepted both offers by pushing the accept button on the messages.</li>
+					<li>Emails were then sent to Ryan and the offerers that the gift was accepted.  This message contains user messages for the users to contact each other internally through the site.</li>
+					<li>Throughout the messaging, Ryan was pleased to find they wanted not only to cook but to eat dinner with Ryan.  He was happy to make friends.</li>
+					<li>Ryan arranged to visit and eat dinner with both of the givers within the next two weeks.</li>
+					<li>He brought dessert.</li>
+					<li>The dinner went well.</li>
+					<li>Bammm!!  The givers Samuel and Pearl were Karma-fied!!</li>
+					<li>Ryan maintained communication with both of them and they had many more dinners.</li>
+					<li>Ryan told other about karma.com and how it felt to be surrounded by like-minded people.</li>
+					<li>Ryan found a profile of a local soccer league looking for coaches.</li
+					<li>Ryan now volunteers on a local soccer team.</li>
+					<li>Bammm! Karma-fied!!</li>
+				</ul>
+				<section>
+					<ul>
+						<li>The soccer team found users wanting to play  soccer on karma.com</li>
+						<li>More Karma-fication!!!</li>
+					</ul>
+				</section>
+		</article>
+	</body>
+</html>
+
+
