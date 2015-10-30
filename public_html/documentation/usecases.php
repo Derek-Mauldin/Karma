558ab987ccdc868e2e67e6a6f9ef2b113ed0dc3c
Jennifer Hung <jennifersukay505@gmail.com>
2015-10-30 14:08:41 -0600

    formatted conceptualmodel.php

---
+++ public_html/documentation/usecases.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+			<meta charset="utf-8"/>
+			<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+			<meta name="viewport" content="width=device-width, initial-scale=1"/>
+			<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"/>
+			<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" rel="stylesheet"/>
+			<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+			<![endif]-->
+			<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/js-cookie/2.0.2/js.cookie.min.js"></script>
+			<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+			<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+			<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.14.0/jquery.validate.min.js"></script>
+			<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.14.0/additional-methods.min.js"></script>
+			<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>k
+			<title>Conceptual Model </title>
+		</head>
+		<body>
+			<header>
+			<h1>User Stories</h1>
+			</header>
+			<main>
+			<article>
+				<dl>
+					<dt>User Story 1</dt>
+					<dd>Role: Average Registered Member</dd>
+					<dd>Story: As a member, I would like to find a member that can give me ride to the dentist.</dd>
+					<dt>User Story 2</dt>
+					<dd>Role: Administrator</dd>
+					<dd>Story: As an administrator, I would like to deactivate accounts of members with ill-intentions.</dd>
+					<dt>User Story 3</dt>
+					<dd>Role: Administrator</dd>
+					<dd>Story: I would like to issue warnings to members that attempt to abuse the site</dd>
+					<dt>User Story 4</dt>
+					<dd>Role: Average Registered Member</dd>
+					-Story: I would like to interact with like-minded, charitable people easily and quickly.
+
+					#User Story 5
+					-Role:Administrator
+					-Story: I would like to be able to thank prolific givers.
+
+					#User Story 6
+					-Role:Administrator
+					-Story:  I would like to get feedback from members
+			</article>
+		</main>
+
+	</body>
+</html>
+
+
