8cb343f793c37cfd98e32d687282b26bce7011fa
Jennifer Hung <jennifersukay505@gmail.com>
2015-10-30 11:07:16 -0600

    testing deployment

--- public_html/documentation/brandon.php
+++ public_html/documentation/brandon.php
@@ -5,6 +5,7 @@
 	</head>
 	<body>
 		<article>
+			<section>
 		<h1>#Persona #1</h1>
 		<dl>
 			<dt>Name</dt>
@@ -19,41 +20,43 @@
 			<dd>Post Graduate, future math professor interested in reading and tutoring, especially
 			students struggling with math</dd>
 		</dl>
+		</section>
 		<section>
-<p>Brandon heard about the Karma website from a fellow University Professor.
-	He decided to join in order to offer tutoring lessons to needy students.
-	This would help him get better at tutoring.</p>
-
-<section>
-<h2>#Use Case 1</h2>
-
-	<ul>Accessed karma.com
-		<li>-Saw listed user profiles and their attributes (giving/in need of).</li>
-		<li>Clicked on “join”, and filled out the small form to sign up.
+			<p>Brandon heard about the Karma website from a fellow University Professor.
+He decided to join in order to offer tutoring lessons to needy students.  This would help him get better at tutoring.</p>
+		</section>
+		<section>
+			<h2>#Use Case 1</h2>
+			<ul>Accessed karma.com
+				<li>-Saw listed user profiles and their attributes (giving/in need of).</li>
+				<li>Clicked on “join”, and filled out the small form to sign up.
 He clicked the check box to receive email notifications from karma.com.
 Received confirmation email, clicked on “confirm”, and came back to karma.com</li>
-		<li>-Filled out his user profile, listing math tutoring as his offered gift for karma.</li>
-		<li>He was not their to receive gifts, but filled out the “need” space
+				<li>-Filled out his user profile, listing math tutoring as his offered gift for karma.</li>
+				<li>He was not their to receive gifts, but filled out the “need” space
 with “A ride to his eye operation on Monday, December 27th…”.</li>
-		<li>Input “math” into the  ‘search” bar with the filter “need” and “closest to location”
+				<li>Input “math” into the  ‘search” bar with the filter “need” and “closest to location”
 to find people on the site affiliated with math. He figured he could tutor in person.
 Though, if no one was close enough, could offer help over the phone or through Skype.
-		<li>Found mother Rebecca of  14-year-old John struggling with math.
-			(The word “math” had been tagged on her profile”).
-aside- profiles contain username,password, and?</li>
-		<li>Clicked on “gift” by her profile</li>
-		<li>Filled out a “gift” note offering a 1-hour-math tutoring lesson and clicked the “Gift $user” button.</li>
-	</ul>
-		<blockquote>3 days later</blockquote>
-	<ul>
-		<li>-Brandon received an email alert that he had mail at karma.com.
+				<li>Found mother Rebecca of  14-year-old John struggling with math.
+The word “math” had been tagged on her profile”).aside- profiles contain username,password, and?</li>
+				<li>Clicked on “gift” by her profile</li>
+				<li>Filled out a “gi-After the test,Brandon received another email from Rebecca, saying that John did super well and thanking him.   Bammm!  Karma-fied!
+ft” note offering a 1-hour-math tutoring lesson and clicked the “Gift $user” button.</li>
+			</ul>
+			<h3>3 days later</h3>
+			<ul>
+				<li>-Brandon received an email alert that he had mail at karma.com.
 He logged on, accessed his inbox, and found a message from Rebeccca.
 She wrote she was excited and thankful and John had an upcoming math test on April 8th.
 Would Brandon be able to tutor soon?
-		</li>
-		<li>Brandon was happy to do so and arranged a Skype math lesson with John for the following evening at 8pm.</li>
-		<li>The lesson took place and was successful.</li>
-		<li> After the test,Brandon received another email from Rebecca, saying that John did super well and thanking him.
-			Bammm!  Karma-fied!
-		</li>
-	</ul>
+				</li>
+				<li>Brandon was happy to do so and arranged a Skype math lesson with John for the following evening at 8pm.</li>
+				<li>The lesson took place and was successful.</li>
+				<li> After the test,Brandon received another email from Rebecca, saying that John did super well and thanking him.
+					Bammm!  Karma-fied!</li>
+				</ul>
+			</section>
+		</article>
+	</body>
+</html>
---
+++ public_html/documentation/entity-relationship.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<title>Does this work</title>
+	</head>
+	<body>
+		<p>Does this work?</p>
+	</body>
+</html>
