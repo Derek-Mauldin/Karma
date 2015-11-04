60b6ecd908a767365ecc6a61df7dbb7ab1c4046f
Jennifer Hung <jennifersukay505@gmail.com>
2015-11-04 11:17:56 -0700

    added updated svg

--- public_html/documentation/svg.php
+++ public_html/documentation/svg.php
@@ -4,6 +4,8 @@
 		<title>svg</title>
 	</head>
 	<body>
+		<p>Switch diamond from member to karma to say "provides" and figure out how to
+		access the svg file in draw.io</p>
 		<img src="../img/karma2.svg">
 	</body>
 </html>
