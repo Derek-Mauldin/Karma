d9a5edee7b8206287c8595c0a46dbd1cd3b2073e
gerald-fongwe <geraldjam2004@yahoo.com>
2015-11-08 21:28:41 -0700

    added accessors and mutators fo rMessage Receiver and MessageContent and messageDate.  https://app.asana.com/0/60993777296285/63432695425249

--- public_html/documentation/data-design/messageclass.php
+++ public_html/documentation/data-design/messageclass.php
@@ -10,17 +10,17 @@
  **/
 class Message {
 	/**
-	 * id for this Tweet; this is the primary key
+	 * id for this Message; this is the primary key
 	 * @var int $messageId
 	 **/
 	private $MessageId;
 	/**
-	 * id of the Profile that sent this Message; this is a foreign key
+	 * id of the messageSender that sent this Message; this is a foreign key
 	 * @var int $MessageSenderId
 	 **/
 	private $MessageSenderId;
 	/**
-	 * id of the Profile that Received this Message; this is a foreign key
+	 * id of the MessageReceiver that Received this Message; this is a foreign key
 	 * @var int $MessageReceiverId
 	 **/
 	private $messageReceiver;
@@ -49,7 +49,7 @@ class Message {
 	 * @throws Exception if some other exception is thrown
 	 **/
 	public function __construct($newMessageId, $newMessageSenderId, $newMessageReceiverId, $newMessageContent,
-										 $newMessageDate = null) {
+	 $newMessageDate = null) {
 		try {
 			$this->setMessageId($newMessageId);
 			$this->setMesaageSenderId($newMessageSenderId);
@@ -112,18 +112,18 @@ class Message {
 	/**
 	 * accessor method for messagesender id
 	 *
-	 * @return int value of profile id
+	 * @return int value of messagesender id
 	 **/
 	public function getMessageSenderId() {
-		return ($this->messageSenderId);
+		return ($this->MessageSenderId);
 	}
 
 	/**
 	 * mutator method for messageSender id
 	 *
-	 * @param int $newMessageId new value of profile id
-	 * @throws InvalidArgumentException if $newMessageId is not an integer or not positive
-	 * @throws RangeException if $newMessageId is not positive
+	 * @param int $newMessageSenderId new value of messagesender id
+	 * @throws InvalidArgumentException if $newMessageSenderId is not an integer or not positive
+	 * @throws RangeException if $newMessageSenderId is not positive
 	 **/
 	public function setMessageSenderId($newMessageSenderId) {
 		// verify the messagesender id is valid
@@ -140,5 +140,108 @@ class Message {
 		// convert and store the messagesender id
 		$this->messagesenderId = intval($newMessageSenderId);
 	}
-}
 
+
+	/**
+	 * accessor method for messageReceiver id
+	 *
+	 * @return mixed value of messageReceiver id
+	 **/
+	public function getMessageReceiverId() {
+		return ($this->MessageReceiverId);
+	}
+
+
+	/**
+	 * mutator method for messageReceiver id
+	 *
+	 * @param int $newMessageReceiverId new value of messageReceiver id
+	 * @throws InvalidArgumentException if $newMessageReceiverId is not an integer or not positive
+	 * @throws RangeException if $newMessagereceiverId is not positive
+	 **/
+	public function setMessageReceiverId($newMessageReceiverId) {
+		// verify the messageReceiver id is valid
+		$newMessageReceiverId = filter_var($newMessageReceiverId, FILTER_VALIDATE_INT);
+		if($newMessageReceiverId === false) {
+			throw(new InvalidArgumentException("messagereceiver id is not a valid integer"));
+		}
+
+		// verify the messagereceiver id is positive
+		if($newMessageReceiverId <= 0) {
+			throw(new RangeException("messagereceiver id is not positive"));
+		}
+
+		// convert and store the messagereceiver id
+		$this->messageReceiverId = intval($newMessageReceiverId);
+	}
+
+	/**
+	 * accessor method for message content
+	 *
+	 * @return string value of message content
+	 **/
+	public function getMessageContent() {
+		return ($this->messageContent);
+	}
+
+
+	/**
+	 * mutator method for message content
+	 *
+	 * @param string $newMessageContent new value of  content
+	 * @throws InvalidArgumentException if $newMessageContent is not a string or insecure
+	 * @throws RangeException if $newMessageContent is > 20000 characters
+	 **/
+	public function setMessageContent($newMessageContent) {
+		// verify the Message content is secure
+		$newMessageContent = trim($newMessageContent);
+		$newMessageContent = filter_var($newMessageContent, FILTER_SANITIZE_STRING);
+		if(empty($newMessageContent) === true) {
+			throw(new InvalidArgumentException("message content is empty or insecure"));
+		}
+
+		// verify the message content will fit in the database
+		if(strlen($newMessageContent) > 20000) {
+			throw(new RangeException("message content too large"));
+		}
+
+		// store the message content
+		$this->messageContent = $newMessageContent;
+	}
+
+	/**
+	 * accessor method for message date
+	 *
+	 * @return DateTime value of message date
+	 **/
+	public function getMessageDate() {
+		return ($this->messageDate);
+	}
+
+	/**
+	 * mutator method for message date
+	 *
+	 * @param mixed $newMessageDate message date as a DateTime object or string (or null to load the current time)
+	 * @throws InvalidArgumentException if $newMessageDate is not a valid object or string
+	 * @throws RangeException if $newMessageDate is a date that does not exist
+	 **/
+	public function setMessageDate($newMessageDate) {
+		// base case: if the date is null, use the current date and time
+		if($newMessageDate === null) {
+			$this->messageDate = new DateTime();
+			return;
+		}
+
+		// store the message date
+		try {
+			$newMessageDate = validateDate($newMessageDate);
+		} catch(InvalidArgumentException $invalidArgument) {
+			throw(new InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+		} catch(RangeException $range) {
+			throw(new RangeException($range->getMessage(), 0, $range));
+		} catch(Exception $exception) {
+			throw(new Exception($exception->getMessage(), 0, $exception));
+		}
+		$this->tweetDate = $newMessageDate;
+	}
+}
