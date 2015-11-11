2a0d0ed32d3eb62ee64edd04571054dbfb7908ff
Jennifer Hung <jennifersukay505@gmail.com>
2015-11-11 09:35:57 -0700

    added to form to home-pg.php
    https://app.asana.com/0/61036105436312/63432295916877

--- public_html/lib/template/home-pg.php
+++ public_html/lib/template/home-pg.php
@@ -15,7 +15,7 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"
 				integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ=="
 				crossorigin="anonymous">
-<!--font-awesome-->
+		<!--font-awesome-->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
 		<!-- Optional theme -->
