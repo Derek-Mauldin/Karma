f4c71565c3a02f4ff2c5851f22e2fc4a59858c9f
Jennifer Hung <jennifersukay505@gmail.com>
2015-11-21 20:53:01 -0700

    updated css

--- public_html/lib/template/home-pg.php
+++ public_html/lib/template/home-pg.php
@@ -8,31 +8,27 @@
 		<meta http-equiv="x-ua-compatible" content="IE=edge"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-		<!--javascript-->
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.js"></script>
 
-		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"
-				integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ=="
-				crossorigin="anonymous">
 
-		<!-- Optional theme -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+		<!-- Bootstrap Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
 
-		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"
-				integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ=="
-				crossorigin="anonymous">
+		<!-- Optional Bootstrap  theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
 
-		<!-- Optional theme -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"
-				integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
+		<!--Font styles-->
+		<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Acme"/>
+
+		<!--Font-awesome-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"/>
+		<!--Animate.css-->
+		<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/animate.css/3.4.0/animate.min.css"/>
 
 		<!--link rel="stylesheet" href="../css/navbar.css">-->
-		<link rel="stylesheet" href="./../css/side-panel.css">
-		<link rel="stylesheet" href="./../css/footer.css">
-		<link rel="stylesheet" href="./../css/nav2.css">
-
+		<link type="text/css" href="../../lib/css/side-panel.css" rel="stylesheet"/>
+		<link type="text/css" href="../../lib/css/footer.css"rel="stylesheet"/>
+		<link type="text/css" href="../../lib/css/nav2.css"rel="stylesheet"/>
+		<link type="text/css" href="../../lib/css/main.css"rel="stylesheet"/>
 
 		<!--HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries-->
 		<!--WARNING: Respond.js doesn't work if you view the page via file://-->
@@ -50,22 +46,19 @@
 
 		<!-- jQuery form validation and additional methods (for capstone) -->
 		<script type="text/javascript" src="//cdnjs.cloudfare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
-		<script type="text/javascript"
-				  src="//cdnjs.cloudfare/com/ajax/libs/jquery-validate/1.14.0/jquery.valiate.min.js"></script>
+		<script type="text/javascript" src="//cdnjs.cloudfare/com/ajax/libs/jquery-validate/1.14.0/jquery.valiate.min.js"></script>
 		<script type="text/javascript"
 				  src="//cdnjs.cloudfare.com/ajax/libs/jquery-validate/1.14.0.additional-methods.min.js"></script>
 
 		<!-- Latest compiled and minified JavaScript -->
-		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"
-				  integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ=="
-				  crossorigin="anonymous"></script>
-
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
 		<!--javascript-->
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.js"></script>
 
 	</head>
-	<body>
-		<div class="row">
+	<body class="sfooter">
+		<div class="sfooter-content">
+			<div class="row">
 			<!-- uncomment code for absolute positioning tweek see top comment in css -->
 			<!-- <div class="absolute-wrapper"> </div> -->
 			<!-- Menu -->
@@ -108,62 +101,88 @@
 								</div>
 							</div>
 						</div>
-
 					</div>
 
 					<!-- Main Menu -->
 					<div class="side-menu-container">
 						<ul class="nav navbar-nav">
-
-
-							<!-- Dropdown-->
-							<li class="panel panel-default" id="dropdown">
-								<a data-toggle="collapse" href="#dropdown-lvl1">$member <span class="caret"></span></a>
-
-								<!-- Dropdown level 1 -->
-								<div id="dropdown-lvl1" class="panel-collapse collapse">
-									<div class="panel-body">
-										<ul class="nav navbar-nav">
-											<li><a href="#">Change Password</a></li>
-											<li><a href="#">Edit Profile</a></li>
-											<li><a href="#">Logout</a></li>
-										</ul>
-							</li>
+							<li><a href="#">Member Handle</a></li>
 							<li><a href="#">Daily Feed</a></li>
+							<li><a href="#">Logout</a></li>
 						</ul>
 					</div><!-- /.navbar-collapse -->
 				</nav>
+
 			</div>
+
+			<!-- Main Content -->
 
 			<!-- Main Content -->
 			<div class="container-fluid">
 				<div class="side-body">
-					<h1> Kismet Main Content here </h1>
+
+				<main>
+					<div class="feed wrapper">
+
+
+					<div class="listing clearfix panel panel-default">
+						<div class="panel-heading"><p><i class="fa fa-times"></i>
+								<span>REQUEST</span>&nbsp;Soccer Coach</p>
+						</div>
+						<div class="container" id="left"><a href="#">
+							<figure>
+								<img class="profileImage" src="http://placehold.it/60x60" alt="thumbnail image">
+								<figcaption>$handle</figcaption>
+							</figure>
+							</a>
+						</div>
+							<div id="right">
+								<button class="btn btn-primary btn-md btn" type="button" data-toggle="modal" data-target="#myModal">Make Offer</button>
+							</div>
+							<div class="listing clearfix panel panel-default">
+							<div id="center">One of a group of parents forming a club soccer team for kids ages 9-12. We would
+like a coach. The season begins in May and we would like to start practice ASAP. We will provide snacks for the team.
+							</div>
+							<div class="panel-offer">
+								<button class="listing-button" type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target="#myModal">
+									Make Offer
+								</button>
+							</div>
+							<!--Modal -->
+							<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+								<div class="modal-dialog" role="document">
+									<?php include_once "../html/form.html" ?>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
-			</div>
-		</div>
 
 
-		<!--Footer -->
-		<footer class="footer">
-			<div class="container">
-				<div class="container center">
-					<ul class="nav">
-						<li class="navbar-text"><a href="#">About Us</a></li>
-						<li class="navbar-text"><a href="#">FAQ</a></li>
-						<li class="navbar-text"><a href="#">Contact Us</a></li>
-						<li class="navbar-text"><a href="#">Terms of Use & Privacy Policy</a></li>
-						<li class="navbar-text"><a href="#">Support</a></li>
-					</ul>
-				</div>
-			</div>
-		</footer>
 
-		<!-- Bootstrap core JavaScript
-		<!-- Placed at the end of the document so the pages load faster -->
-		<script src="./../js/side-panel.js"></script>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-		<script src="../../dist/js/bootstrap.min.js"></script>
-		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-	</body>
+				<!--.sfooter-content-->
+
+				<footer class="footer navbar-fixed-bottom">
+					<div class="container">
+						<ul class="nav">
+							<li class="navbar-text"><a href="#">About Us</a></li>
+							<li class="navbar-text"><a href="#">FAQ</a></li>
+							<li class="navbar-text"><a href="#">Contact Us</a></li>
+							<li class="navbar-text"><a href="#">Terms of Use & Privacy Policy</a></li>
+							<li class="navbar-text"><a href="#">Support</a></li>
+						</ul>
+					</div>
+				</footer>
+					</body>
+
+							<!-- Bootstrap core JavaScript
+							<!-- Placed at the end of the document so the pages load faster -->
+							<script type="text/javascript" src="../js/form-scripts.js"></script>
+
+							<script src="./../js/side-panel.js"></script>
+							<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+							<script src="../../dist/js/bootstrap.min.js"></script>
+
 </html>
+
+
