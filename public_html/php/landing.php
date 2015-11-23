5f47807ec52d7f7cfbe1eda91021ffb3445b066c
Jennifer Hung <jennifersukay505@gmail.com>
2015-11-23 10:36:32 -0700

    updated forms,landing pages, and css

---
+++ public_html/php/landing.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+
+		<meta charset="utf-8">
+		<meta http-equiv="x-ua-compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<!--javascript-->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.js"></script>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"
+				integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ=="
+				crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"
+				integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
+		  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-social/4.10.1/bootstrap-social.min.css ">
+
+		<!--font awesome-->
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+
+		<!--Link to custom CSS files here-->
+		<link type="text/css" href="../lib/css/navbar.css" rel="stylesheet">
+		<link type="text/css" href="../lib/css/landing.css" rel="stylesheet">
+		<!--HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries-->
+		<!--WARNING: Respond.js doesn't work if you view the page via file://-->
+		<!--[if lt IE 9]>
+		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!--js-cookie (for capstone) -->
+		<script type="text/javascript" src="//cdnjs.cloudfare.com/ajax/libs/js-cookie/2.0.2/js.cookie.min.js"></script>
+
+		<!--jQuery (necessary for bootstrap's javaScript plugins)-->
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+
+		<!-- jQuery form validation and additional methods (for capstone) -->
+		<script type="text/javascript" src="//cdnjs.cloudfare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript"
+				  src="//cdnjs.cloudfare/com/ajax/libs/jquery-validate/1.14.0/jquery.valiate.min.js"></script>
+		<script type="text/javascript"
+				  src="//cdnjs.cloudfare.com/ajax/libs/jquery-validate/1.14.0.additional-methods.min.js"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"
+				  integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ=="
+				  crossorigin="anonymous"></script>
+
+
+		<title>Simple Karma Template</title>
+
+
+	</head>
+
+
+	<body>
+		<nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation">
+			<div class="container topnav">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse"
+							  data-target="#bs-example-navbar-collapse-1">
+
+
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand topnav" href="#">Kismet</a>
+				</div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav navbar-right">
+
+
+						<li>
+							<a href="#Kismet">Welcome</a>
+						</li>
+						<li>
+							<a href="#about">About</a>
+						</li>
+
+						<li><a href="register.php">Sign in</a></li>
+					</ul>
+				<!-- /.navbar-collapse -->
+			</div>
+			<!-- /.container -->
+		</nav>
+
+
+		<!-- Header -->
+		<a name="about"></a>
+
+		<div class="intro-header">
+			<div class="container">
+
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="intro-message">
+							<h1>Kismet</h1>
+
+							<h3>Slogan</h3>
+							<hr class="intro-divider">
+							<ul class="list-inline intro-social-buttons">
+									<li><a class="btn btn-lg btn-social btn-twitter">
+											<span class="fa fa-twitter"></span> Sign in with Twitter</a></li>
+									<li><a class="btn btn-lg btn-social btn-facebook">
+											<span class="fa fa-facebook"></span> Sign in with Facebook</a></li>
+									<li>
+										<a href="#" class="btn btn-lg btn-social btn-linkedin"><i class="fa fa-linkedin"></i>
+											<span class="network-name">Linkedin</span></a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+
+			</div>
+			<!-- /.container -->
+
+		</div>
+		<!-- /.intro-header -->
+
+		<!-- Page Content -->
+
+		<a name="about"></a>
+
+		<div class="content-section-a">
+
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-5 col-sm-6">
+						<hr class="section-heading-spacer">
+						<div class="clearfix"></div>
+						<h2 class="section-heading" id="About">Description</h2>
+					</div>
+					<div class="col-lg-5 col-lg-offset-2 col-sm-6">
+						<img class="img-responsive" src="../img/robot-logo.png" alt="">
+					</div>
+				</div>
+
+			</div>
+			<!-- /.container -->
+
+		</div>
+		<!-- /.content-section-a -->
+		<!-- /.content-section-b -->
+		>
+		<!-- /.content-section-a -->
+
+		<a name="contact"></a>
+
+		<div class="banner">
+
+			<div class="container">
+
+				<div class="row">
+					<div class="col-lg-6">
+						<h2>Text</h2>
+					</div>
+				</div>
+			</div>
+			<!-- /.container -->
+		</div>
+		<!-- /.banner -->
+
+		<!-- Footer -->
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<ul class="list-inline">
+							<li>
+								<a href="#">Home</a>
+							</li>
+							<li class="footer-menu-divider">&sdot;</li>
+							<li>
+								<a href="#about">About</a>
+							</li>
+							<li class="footer-menu-divider">&sdot;</li>
+							<li>
+								<a href="#services">Contact</a>
+							</li>
+							<li class="footer-menu-divider">&sdot;</li>
+						</ul>
+						<p class="copyright text-muted small">Copyright &copy; Kismet 2015. All Rights Reserved</p>
+					</div>
+				</div>
+			</div>
+		</footer>
+
+		<!-- jQuery -->
+		<script src="js/jquery.js"></script>
+
+		<!-- Bootstrap Core JavaScript -->
+		<script src="js/bootstrap.min.js"></script>
+
+	</body>
+
+</html>
---
+++ public_html/php/recover.php
@@ -0,0 +1,160 @@
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<!--javascript-->
+				<script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.js"></script>
+
+				<!-- Latest compiled and minified CSS -->
+				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+
+				<!-- Optional theme -->
+				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
+
+				<!--font awesome-->
+				<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+				<!--Link to custom CSS files here-->
+				<link type="text/css" href="../lib/css/navbar.css" rel="stylesheet">
+				<link type="text/css" href="../lib/css/register.css" rel="stylesheet">
+
+				<!--HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries-->
+				<!--WARNING: Respond.js doesn't work if you view the page via file://-->
+				<!--[if lt IE 9]>
+				<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+				<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+				<![endif]-->
+
+				<!--js-cookie (for capstone) -->
+				<script type="text/javascript" src="//cdnjs.cloudfare.com/ajax/libs/js-cookie/2.0.2/js.cookie.min.js"></script>
+
+				<!--jQuery (necessary for bootstrap's javaScript plugins)-->
+				<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+
+				<!-- jQuery form validation and additional methods (for capstone) -->
+				<script type="text/javascript" src="//cdnjs.cloudfare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+				<script type="text/javascript" src="//cdnjs.cloudfare/com/ajax/libs/jquery-validate/1.14.0/jquery.valiate.min.js"></script>
+				<script type="text/javascript" src="//cdnjs.cloudfare.com/ajax/libs/jquery-validate/1.14.0.additional-methods.min.js"></script>
+
+				<!-- Latest compiled and minified JavaScript -->
+				<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+
+				<title>Kismet - Recover</title>
+	</head>
+		<!-- Fixed navbar -->
+		<div class="navbar navbar-default navbar-fixed-top" role="navigation">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand brand-shifted" href="#"><img class="brand-icon" src="../img/orange-robot-logo-sm.png" alt="Kismet Logo"></a>
+				</div>
+				<!-- Navbar collapse -->
+				<div class="navbar-collapse collapse">
+					<ul class="nav navbar-nav">
+						<li class=""><a href="#">Kismet</a></li>
+						<li class=""><a href="#">Contact</a></li>
+
+					</ul>
+					<ul class="nav navbar-nav navbar-right shifted">
+						<li class=""><a href="#">Register</a></li>
+						<li class=""><a href="#">Log In</a></li>
+					</ul>
+				</div><!--/.nav-collapse -->
+			</div>
+		</div><!-- /.Fixed navbar -->
+	<div class="clearfix"</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-lg-offset-3">
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3">
+					<div class="alert-placeholder"></div>
+					<div class="panel panel-success">
+						<div class="panel-body">
+							<div class="row">
+								<div class="col-lg-12">
+									<div class="text-center"><h2><b>Recover Account</b></h2></div>
+									<form id="register-form" action="#" method="post" role="form" autocomplete="off">
+										<div class="form-group">
+											<label for="email">Email Address</label>
+											<input type="email" name="email" id="email" tabindex="1" class="form-control" placeholder="Email Address" value="" autocomplete="off" required/>
+										</div>
+										<div class="form-group">
+											<div class="row">
+												<div class="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-12 col-xs-12">
+													<input type="submit" name="recover-submit" id="recover-submit" tabindex="2" class="form-control btn btn-success" value="Recover Account" />
+												</div>
+											</div>
+										</div>
+										<input type="hidden" class="hide" name="token" id="token" value="b49912e5e03ef179a45cded02fdc1ecc">
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div> <!-- /container -->
+	<div class="container-fluid">
+			<br>
+			<div class="row">
+
+				<div class="col-lg-3"></div>
+				<div class="col-lg-3"></div>
+				<div class="col-lg-3"></div>
+			</div>
+		</div>
+		<!-- Core Javascript -->
+		<script src="http://cdn.phpoll.com/js/main.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
+		<script>
+			window.onload = function(){
+				if (typeof pieChartData !== 'undefined') {
+					var ctx = document.getElementById("chart-area").getContext("2d");
+					window.myPie = new Chart(ctx).Pie(pieChartData,{
+						animationEasing: "easeOutQuart",
+						responsive: true,
+						tooltipTemplate: "\"<%= label %>\" (<%= value %>%)"
+					});
+				}
+				$('.validate-input').inputValidator();
+			};
+			(function($) {
+				$(document).ready(function() {
+
+					var cookieHeader = $('<a/>').attr('href', '#').attr('title', 'Click To Close').css({
+						'padding': '8px',
+						'position': 'fixed',
+						'bottom': '0',
+						'left': '0',
+						'width': '100%',
+						'background-color': '#353535',
+						'color': '#FFF',
+						'text-align': 'center'
+					}).css('z-index', '9999').text('This site uses cookies to enhance user experience').addClass('close-cookie-banner');
+					if ($.cookie('allow-cookies') === undefined) {
+						$('body').append(cookieHeader);
+					}
+					$(document).on('click', '.close-cookie-banner', function(e) {
+						e.preventDefault();
+						$.cookie('allow-cookies', true, { expires: 1000, path: '/' });
+						$(this).remove();
+					});
+
+				});
+			})(jQuery);
+		</script>
+	</body>
+</html>
