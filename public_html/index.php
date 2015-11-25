ce905aa6ed0ff9a20447134efdf5db163f085170
Jennifer Hung <jennifersukay505@gmail.com>
2015-11-25 16:58:45 -0700

    https://app.asana.com/0/60993777296285/63432295916877 rlewis37 add head-utils.php

---
+++ public_html/index.php
@@ -0,0 +1,12 @@
+<?php
+/*grab current directory*/
+$CURRENT_DIR = __DIR__;
+
+/*set page title here*/
+$PAGE_TITLE = "Welcome to Karma";
+
+/*load head-utils.php*/
+require_once("php/template/head-utils.php");
+?>
+
+
