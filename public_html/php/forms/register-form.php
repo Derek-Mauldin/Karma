3cfac3782bb3ccf5e8127b9032c80a76fefe5461
Derek Mauildin <derek.e.mauldin@gmail.com>
2015-11-25 10:40:57 -0700

    fixed contact-us-form.php https://app.asana.com/0/60993777296285/63432295916877

--- public_html/php/forms/register-form.php
+++ public_html/php/forms/register-form.php
@@ -71,7 +71,7 @@
 				<div class="col-sm-6 col-md-6">
 					<form class="navbar-form" role="search">
 						<div class="input-group">
-							<input type="text" class="form-control" placeholder="Search" name="q">
+							<input type="text" class="form-control" placeholder="Search" name="search" id="search">
 							<div class="input-group-btn">
 								<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
 							</div>
