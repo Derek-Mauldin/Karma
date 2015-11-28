8e7aef92f516f3bb34190e9e29f8cb338156f1ba
Derek Mauildin <derek.e.mauldin@gmail.com>
2015-11-27 19:00:21 -0700

    added first name and last name fields to register-form finished first pass on register-controller https://app.asana.com/0/60993777296285/68642071997916

--- public_html/php/controllers/register-controller.php
+++ public_html/php/controllers/register-controller.php
@@ -1,11 +1,18 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: derekmauldin
- * Date: 11/27/15
- * Time: 12:39 PM
  *
- * controller for Karma user registration
+ * controller for Karma register-form
+ *
+ * @throws InvalidArgumentException if the form is incomplete
+ * @throws InvalidArgumentException for invalid firstName
+ * @throws InvalidArgumentException for invalid lastName
+ * @throws InvalidArgumentException for invalid userName
+ * @throws InvalidArgumentException for invalid email
+ * @throws InvalidArgumentException for invalid password
+ * @throws InvalidArgumentException for invalid confirm-password
+ * @throws InvalidArgumentException if password and confirm-password do not match
+ * @throws InvalidArgumentException if userName already exists in the database
+ *
  */
 
 require_once(dirname(__DIR__) . "/classes/autoload.php");
@@ -23,14 +30,34 @@ try {
 	verifyXsrf();
 
 	// ensures that the form fields are filled out
-	if(@isset($_POST["email"]) === false ||
+	if(@isset($_POST["firstName"]) === false ||
+		@isset($_POST["lastName"]) === false ||
+		@isset($_POST["userName"]) === false ||
+		@isset($_POST["email"]) === false ||
 		@isset($_POST["password"]) === false ||
 		@isset($_POST["confirm-password"]) === false
 	) {
 		throw(new InvalidArgumentException("The form is not complete. Please verify and try again"));
 	}
 
-	// sanitize user input
+	// trim input
+	trim($_POST["firstName"]);
+	trim($_POST["lastName"]);
+	trim($_POST["userName"]);
+	trim($_POST["email"]);
+	trim($_POST["password"]);
+	trim($_POST["confirm-password"]);
+
+	// sanitize  input
+	if(filter_var($_POST["firstName"], FILTER_SANITIZE_STRING) === false){
+		throw(new InvalidArgumentException("invalid First Name"));
+	}
+	if(filter_var($_POST["lastName"], FILTER_SANITIZE_STRING) === false){
+		throw(new InvalidArgumentException("invalid Last Name"));
+	}
+	if(filter_var($_POST["userName"], FILTER_SANITIZE_STRING) === false){
+		throw(new InvalidArgumentException("invalid userName"));
+	}
 	if(filter_var($_POST["email"], FILTER_SANITIZE_EMAIL) === false){
 		throw(new InvalidArgumentException("invalid email"));
 	}
@@ -41,33 +68,46 @@ try {
 		throw(new InvalidArgumentException("invalid password confirmation"));
 	}
 
+
 	// verify that the password and password confirmation fields contain the same value
 	if($_POST["password"] !== $_POST["confirm-password"]) {
 		throw(new InvalidArgumentException("password and confirmation do not match"));
 	}
+
+
+	// check to see if user handle is already being used by a profile
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/karma.ini");
+	$profile = Profile::getProfileByProfileHandle($pdo, $_POST["userName"]);
+	if(is_object($profile) === true) {
+		throw(new InvalidArgumentException("Try a different User Name"));
+	}
+
 
 	// create a salt, hash, and email activation code for a new member
 	$salt = bin2hex(openssl_random_pseudo_bytes(164));
 	$hash = hash_pbkdf2("sha512", $_POST["password"], $SALT, 4096, 255);
 	$emailActivation = bin2hex(openssl_random_pseudo_bytes(16));
 
+
 	// create a new member and insert into mysql
-	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/karma.ini");
 	$member = new member(null, "s", $_POST["email"], $emailActivation, $hash, $salt);
 	$member->insert($pdo);
 
 
+	// create a new profile with the new member ID and insert into mysql
+	$profile = new Profile(null, $member->getMemberId(), "", $_POST["userName"], $_POST["firstName"], $_POST["lastName"]);
+	$profile->insertProfile($pdo);
 
 
+	// create message for email activation
+	$message = <<< EOF
+<h1>This is an Important Message about your Account Activation</h1>
+<p>To certify this email address and activate your account, please visit: <a href="$confirmLink">Confirmation</a></p>
+EOF;
 
 
-
-
-
-
-
-
-
+	// send confirmation email to new member
+	sendEmail($_POST["email"], $_POST["firstName"], $_POST["lastName"] ,$message);
 
 
 }catch (Exception $e) {
