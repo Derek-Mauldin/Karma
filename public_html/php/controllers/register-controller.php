212aab07cd9b04ffc8ef4acc9e791cc40ce34c54
Derek Mauildin <derek.e.mauldin@gmail.com>
2015-11-27 17:06:09 -0700

    added mailer.php https://app.asana.com/0/60993777296285/68642071997916

--- public_html/php/controllers/register-controller.php
+++ public_html/php/controllers/register-controller.php
@@ -11,7 +11,7 @@
 require_once(dirname(__DIR__) . "/classes/autoload.php");
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 require_once(dirname(dirname(__DIR__)) . "/lib/php/xsrf.php");
-require_once(dirname(dirname(dirname(__DIR__))) . "/vendor/autoload.php");
+require_once(dirname(dirname(__DIR__)) . "/lib/php/mailer.php");
 
 
 try {
