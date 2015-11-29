53d3081ca55d969db6259f60146664053477f903
Derek Mauildin <derek.e.mauldin@gmail.com>
2015-11-29 16:06:42 -0700

    cosmetic adjustments

--- public_html/php/forms/message-form.php
+++ public_html/php/forms/message-form.php
@@ -38,7 +38,7 @@
 		<!-- The div class="form-wrap" is the black box containing the form. It's set to a column width of 12 for small screens, and a column width of 6 for medium screens on up -->
 		<div class="col-xs-12 col-md-7 form-wrap">
 			<!-- Form is centered within it's container, and is set to 10 be columns wide RELATIVE TO IT'S CONTAINER, and offset to the right by one column. See classes: col-xs-offset-1 & col-xs-10 -->
-			<form method="get" action="#" id="contact-us" name="contact-us" class="form-horizontal col-xs-10 col-xs-offset-1">
+			<form method="get" action="../controllers/message-controller.php" id="karmaMessage" name="karmaMessage" class="form-horizontal col-xs-10 col-xs-offset-1">
 
 				<div class="form-group">
 					<!-- Labels for each field are places within a <label> tag. Use the "for" attribute. the class="control-label" is for styling. -->
