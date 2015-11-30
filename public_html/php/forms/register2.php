d2841059ad0fdf80c06ab1dc47f0975be21bd5b2
Derek Mauildin <derek.e.mauldin@gmail.com>
2015-11-30 14:24:50 -0700

    cosmetic adjustments

--- public_html/php/forms/register2.php
+++ public_html/php/forms/register2.php
@@ -1,51 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
+<?php
+/*grab current directory*/
+$CURRENT_DIR = __DIR__;
 
-		<meta charset="utf-8">
-		<meta http-equiv="x-ua-compatible" content="IE=edge"/>
-		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+/*set page title here*/
+$PAGE_TITLE = "Welcome to Karma";
 
-		<!--javascript-->
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.js"></script>
+/*load head-utils.php*/
+require_once("../template/head-utils.php");
+?>
 
-		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
 
-		<!-- Optional theme -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
-
-		<!--Font awesome-->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-
-		<!-- Custom Fonts -->
-		<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-		<!--Link to custom CSS files here-->
-		<link rel="stylesheet" href="../../lib/css/styles.css">
-		<!--HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries-->
-		<!--WARNING: Respond.js doesn't work if you view the page via file://-->
-		<!--[if lt IE 9]>
-		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<![endif]-->
-
-		<!--js-cookie (for capstone) -->
-		<script type="text/javascript" src="//cdnjs.cloudfare.com/ajax/libs/js-cookie/2.0.2/js.cookie.min.js"></script>
-
-		<!--jQuery (necessary for bootstrap's javaScript plugins)-->
-		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-
-		<!-- jQuery form validation and additional methods (for capstone) -->
-		<script type="text/javascript" src="//cdnjs.cloudfare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
-		<script type="text/javascript" src="//cdnjs.cloudfare/com/ajax/libs/jquery-validate/1.14.0/jquery.valiate.min.js"></script>
-		<script type="text/javascript" src="//cdnjs.cloudfare.com/ajax/libs/jquery-validate/1.14.0.additional-methods.min.js"></script>
-
-		<!-- Latest compiled and minified JavaScript -->
-		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
-		<script type="text/javascript"src="../../lib/js/site-scripts.js"></script>
-	</head>
-	<body>
 		<div class="container">
 			<div class="row">
 				<div class="col-md-6 col-md-offset-3">
@@ -66,7 +30,7 @@
 							<div class="row">
 								<div class="col-lg-12">
 
-									<form id="login-form" name="login-form" action="../controllers/login-controller.php" method="post" role="form" style="display: block;">
+									<form id="login-form" name="login-form" action="<?php echo $PREFIX;?>php/controllers/login-controller.php" method="post" role="form" style="display: block;">
 										<div class="form-group">
 											<input type="text" name="email" id="email" tabindex="1" class="form-control" placeholder="Email" value="">
 										</div>
