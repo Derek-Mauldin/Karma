34aea93cbe6f6ea7d99f2166a21c58915ea494cc
Evan Smith <esmith49@cnm.edu>
2015-11-30 15:05:23 -0700

    edit controler for edit profile to include member https://app.asana.com/0/60993777296285/68642071997924

--- public_html/php/controllers/edit-profile-controller.php
+++ public_html/php/controllers/edit-profile-controller.php
@@ -16,49 +16,53 @@
 		//Check if the profileId passed from the form is valid and then get the profile if okay.
 		if(@isset($memberId) !== false){
 			$profile = Profile::getProfileByMemberId($pdo, $memberId);
+			$member = Member::getMemberByMemberId($pdo, $memberId);
 		} else {
 			throw(new InvalidArgumentException('Member id not found or is invalid.'));
 		}
 
 		//Check if the user input values are valid
-		if(/*@isset($_POST['profileBlurb']) 	=== false ||*/
+		if(@isset($_POST['password']) 	=== false ||
 			@isset($_POST['profileHandle']) 	=== false ||
 			@isset($_POST['firstName']) 		=== false ||
-			@isset($_POST['lastName'])			=== false){
+			@isset($_POST['lastName'])			=== false ||
+			@isset($_POST["email"])				=== false ||
+			@isset($_POST["confirm-password"])=== false) {
+
 			throw(new InvalidArgumentException('The form is not complete or is missing inputs'));
-		} else {
-			if(@isset($_POST['profileBlurb']) === true) {
-				$profileBlurb = Filter::filterString($_POST['profileBlurb'], 'profileBlurb');
-			} else {
-				$profileBlurb = null;
-			}
+		}
+
 			if(array_key_exists($_POST, "profileBlurb")) {
 				$profileBlurb =  Filter::filterString($_POST['profileBlurb'], 'profileBlurb');
 			}
 			$profileHandle 	= Filter::filterString($_POST['profileHandle'], 'profileHandle');
 			$firstName 			= Filter::filterString($_POST['firstName'], 'firstName');
 			$lastName 			= Filter::filterString($_POST['lastName'], 'lastName');
-		}
+			$email				= Filter::filterEmail($_POST['email'], 'email');
+			$password			= Filter::filterString($_POST['password'], 'password');
+			$confirmPassword = Filter::filterString($_POST['confirm-password'], 'confirm-password');
 
-		if($profile === null) {
-			throw(new InvalidArgumentException("profile for this member id does not exist"));
+
+		if(($profile === null) || ($member === null)) {
+			throw(new InvalidArgumentException("profile or member does not exist for the member id"));
 		}
 
 		//Verify that the handle has not been taken by another user
 		$checkProfileHandle = Profile::getProfileByProfileHandle($pdo, $profileHandle);
-		if($checkProfileHandle !== null && $checkProfileHandle->getProfileId() !== $profile->getProfileId()){
+		if($checkProfileHandle !== null){
 			throw(new InvalidArgumentException('The handle you have chosen has already been taken by another user.'));
-		} else if($profile->getProfileId() === null) {
-//			$profile->insertProfile($pdo);
-			throw(new InvalidArgumentException('The profile does not exist'));
-		} else {
-			//Set the profiles values to the form values
+		}
+
+
+			//Set the profiles values and member values to the form values
 			$profile->setProfileBlurb($profileBlurb);
 			$profile->setProfileHandle($profileHandle);
 			$profile->setProfileFirstName($firstName);
 			$profile->setProfileLastName($lastName);
 			$profile->updateProfile($pdo);
-		}
+		   $member->setEmail($email);
+		   $member->update($pdo);
+
 
 	}catch (Exception $exception){
 		echo "<p class=\"alert alert-danger\">Exception: " . $e->getMessage() . "</p>";
