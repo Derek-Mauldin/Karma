0f4dabe4a57f119350f81444d10bb43dfb604730
Jennifer Hung <jennifersukay505@gmail.com>
2015-11-30 13:32:35 -0700

    https://app.asana.com/0/61036105436312/68642071997926 added need-controller.js, updated need-controller.php and need-form.php

--- public_html/php/forms/register2.php
+++ public_html/php/forms/register2.php
@@ -53,7 +53,7 @@
 						<div class="panel-heading">
 							<div class="row">
 								<div class="col-xs-6">
-									<a href="login-form.php" class="active" id="login-form-link">Login</a>
+									<a href="#login-form" class="active" id="login-form-link">Login</a>
 								</div>
 								<div class="col-xs-6">
 									<a href="#" id="register-form-link">Register</a>
