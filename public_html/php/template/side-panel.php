10d974f2a70562677aa21267c0f92783c6b8897f
Jennifer Hung <jennifersukay505@gmail.com>
2015-12-02 11:49:25 -0700

    updated fside-panel.php. edit-profile.php. home-pg-side-toggle-testing.php. recover-form.php. and index.php

--- public_html/php/template/side-panel.php
+++ public_html/php/template/side-panel.php
@@ -1,5 +1,5 @@
 <section id="menu" class="side-panel panel panel-default">
-	<a class="sidebar-brand" href="#"><img id="logo" src="../../img/robot-logo.png"/></a>
+	<a class="sidebar-brand" href="#"><img id="logo" src="../../img/octopus-logo.png"/></a>
 
 	<!--side panel body links-->
 	<div id="hp" onclick="HideAllShowOne('hp')">
@@ -18,5 +18,9 @@
 	<div id="mp" onclick="HideAllShowOne('fp')">
 		<a class="lead" href="javascript:ReverseDisplay('feed-page')">Feed</a>
 
+	</div>
+
+	<div id="lp" onclick="HideAllShowOne('lp)">
+		<a class="lead" href="javascript:ReverseDisplay('logout-page')">Logout</a>
 	</div>
 </section>
