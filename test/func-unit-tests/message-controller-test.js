8adbcc9301658dea3404c2dca452f8848f88ad6e
gerald-fongwe <geraldjam2004@yahoo.com>
2015-12-04 10:18:08 -0700

    added invalid conditions in func-unit-test for message controller.php, https://app.asana.com/0/60993777296285/68642071997922

--- test/func-unit-tests/message-controller-test.js
+++ test/func-unit-tests/message-controller-test.js
@@ -9,6 +9,7 @@ module("tabs", {
 var INVALID_MESSAGESENDER    = "7";
 var INVALID_MESSEGERECEIVER = "8";
 var INVALID_KARMAMESSAGE = "message";
+
 var VALID_MESSAGESENDER = "1";
 var VALID_MESSAGERECEIVER = "2";
 var VALID_KARMAMESSAGE = "message sent to superman";
@@ -39,3 +40,28 @@ function testValidFields() {
 
 /**
  * test filling in invalid form data
+ **/
+function testInValidFields() {
+	// fill in the form values
+	F("#messageSender").type(INVALID_MESSAGESENDER);
+	F("#messageReceiver").type(INVALID_MESSEGERECEIVER);
+	F("#karmaMessage").type(INVALID_KARMAMESSAGE);
+
+	// click the button once all the fields are filled in
+	F("#Submit").click();
+
+	// in forms, we want to assert the form worked as expected
+	// here, we assert we got the success message from the AJAX call
+	F(".alert").visible(function() {
+		// create a regular expression that evaluates the successful text
+		var successRegex = /message \(id = \d+\) posted!/;
+
+		// the ok() function from qunit is equivalent to SimpleTest's assertTrue()
+		ok(F(this).hasClass("alert-success"), "successful alert CSS");
+		ok(successRegex.test(F(this).html()), "successful message");
+	});
+}
+
+// the test function *MUST* be called in order for the test to execute
+test("test valid fields", testValidFields);
+test("test invalid fields", testInvalidFields);
