34eb1207328b5ae5a63361063f293951453f9b1a
JenniferSukay505 <JenniferSukay505@gmail.com>
2015-12-07 15:17:22 -0700

    updated index.php

--- public_html/php/template/footer.php
+++ public_html/php/template/footer.php
@@ -1,10 +1,11 @@
 <div class="container">
-
-			<ul class="nav">
-				<li class="navbar-text"><a href="#">About Us</a></li>
-				<li class="navbar-text"><a href="#">FAQ</a></li>
-				<li class="navbar-text"><a href="#">Contact Us</a></li>
-				<li class="navbar-text"><a href="#">Terms of Use</a></li>
-				<li class="navbar-text"><a href="#">Support</a></li>
-			</ul>
-		</div>
+	<div class="row">
+		<ul class="nav">
+			<li class="navbar-text"><a href="#">About Us</a></li>
+			<li class="navbar-text"><a href="#">FAQ</a></li>
+			<li class="navbar-text"><a href="#">Contact Us</a></li>
+			<li class="navbar-text"><a href="#">Terms of Use</a></li>
+			<li class="navbar-text"><a href="#">Support</a></li>
+		</ul>
+	</div>
+</div>
