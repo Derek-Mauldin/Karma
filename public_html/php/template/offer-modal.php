a09894bf980b14c512e7bc14fe8b30e4f4e2032c
Jennifer Hung <jennifersukay505@gmail.com>
2015-12-14 08:45:15 -0700

    added to index, head-utils

--- public_html/php/template/offer-modal.php
+++ public_html/php/template/offer-modal.php
@@ -2,6 +2,7 @@
 	<div class="modal-dialog"> 
 		<div class="modal-content">
 			<div class="modal-header">  
+
 				<button type="button" class="close" data-dismiss="modal" aria-label="close"><span hidden="true">&times;</span></button> 
 				<h3 class="modal-title">Give $user a hand</h3>  
 			</div> 
@@ -11,6 +12,7 @@
 						<textarea id="message" class="form-control" rows="5"  placeholder="Hi! I can can give you a Skype algebra tutoring session this Friday or Saturday afternoon."  required></textarea>  
 						<div class="help-block with-errors"></div> 
 					</div> 
+
 					<button type="submit" id="form-submit"  class="btn btn-success btn-lg"> Offer !</button>
 					<button type="button" class="btn btn-default btn-lg"  data-dismiss="modal" aria-hidden="true"> Cancel</button>
 					 <div id="msgSubmit" class="h3 text-center hidden"></div> 
---
+++ public_html/php/template/success.php
@@ -0,0 +1,4 @@
+<div class="col-md-12" id="registration-complete">
+		<h1 class="panel-title" ><i class="fa fa-times pull-right" id="hide"></i>
+			Karma Activation
+		</h1>
