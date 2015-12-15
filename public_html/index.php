77df1b34a1aff8f6d00147d7af7172a4633a73f7
Jennifer Hung <jennifersukay505@gmail.com>
2015-12-14 17:15:20 -0700

    edited index

--- public_html/index.php
+++ public_html/index.php
@@ -222,29 +222,37 @@ require_once("php/template/head-utils.php");
 										<div class="box-header with-border">
 											<h3 class="box-title">Compose New Message</h3>
 										</div><!-- /.box-header -->
+
 										<div class="box-body">
 											<div class="form-group">
 												<input class="form-control" placeholder="To:">
-										</div>
+										</div><!--/form-group-->
+
 										<div class="form-group">
 											<input class="form-control" placeholder="Subject:">
-										</div>
+										</div><!--/form-group-->
+
 										<div class="form-group">
                     					<textarea id="compose-textarea" class="form-control" style="height: 150px"></textarea>
-										</div>
+										</div><!--/form-group-->
+
 										<div class="form-group">
 											<div class="btn btn-default btn-file">
 												<i class="fa fa-paperclip"></i> Attachment
 												<input type="file" name="attachment">
-											</div>
+											</div><!--/btn-default-->
 											<p class="help-block">Max. 32MB</p>
-										</div>
-									</div><!-- /.box-body -->
-									<div class="box-footer">
+										</div><!--/form-group-->
+									</div><!-- /.box-body-->
+
+	<!----------------------------------mailbox compose footer----------------------------------------------------------->
+
+										<div class="box-footer">
 										<div class="pull-right">
 											<button class="btn btn-default"><i class="fa fa-pencil"></i> Draft</button>
 											<button type="submit" class="btn btn-primary"><i class="fa fa-envelope-o"></i> Send</button>
-										</div>
+										</div><!--/pull-right-->
+
 										<button class="btn btn-default"><i class="fa fa-times"></i> Discard</button>
 									</div><!-- /.box-footer -->
 								</div><!-- /. box -->
@@ -265,11 +273,11 @@ require_once("php/template/head-utils.php");
 												<div class="has-feedback">
 													<input type="text" class="form-control input-sm" placeholder="Search Mail">
 													<span class="glyphicon glyphicon-search form-control-feedback"></span>
-												</div>
+												</div><!--/has-feeedback-->
 											</div><!-- /.box-tools -->
 										</div><!-- /.box-header -->
 
-	<!--------------------------------------------------------sent controls-------------------------->
+	<!-----------------------------------------------sent controls------------------------------------------->
 
 										<div class="box-body no-padding">
 											<div class="mailbox-controls">
@@ -337,29 +345,26 @@ require_once("php/template/head-utils.php");
 													<div class="btn-group">
 														<button class="btn btn-default btn-sm"><i class="fa fa-chevron-left"></i></button>
 														<button class="btn btn-default btn-sm"><i class="fa fa-chevron-right"></i></button>
+
 													</div><!-- /.btn-group -->
 												</div><!-- /.pull-right -->
-											</div>
-										</div>
+											</div><!--/mailbox-controls-->
+										</div><!--/box-footer-->
 									</div><!-- /. box -->
 								</div><!-- /.sent -->
 							</div><!-- / #sent-wrapper -->
 						</div><!-- /.mailbox-->
 					</div><!-- /.mailbox-body -->
-
-
-
-						</div><!--/.panel-body #mailbox-wrapper-->
-					</div><!--/.panel-default #mailbox-body-->
-				</div><!--/.mailbox-->
-			</div><!--/.panel-primary #mailbox-header-wrapper-->
+				</div><!--/.panel-body #mailbox-wrapper-->
+			</div><!--/.panel-default #mailbox-body-->
+		</div><!--/.mailbox-->
+	</div><!--/.panel-primary #mailbox-header-wrapper-->
 
 	<!-------------------------------------------edit-profile-page------------------------------------------->
 
-				<div class=edit-profile-page" id="edit-profile-page" style="display:none;">
-
-				<div class="row" id="edit-profile-wrapper">
-					<div class="col-md-6 col-md-offset-3">
+		<div class=edit-profile-page" id="edit-profile-page" style="display:none;">
+			<div class="row" id="edit-profile-wrapper">
+				<div class="col-md-6 col-md-offset-3">
 
 	<!-----------------------------------------edit profile/settings links----------------------------------->
 
