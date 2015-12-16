e763c7aeb6ececc7c408428755a6a71388415947
Derek Mauildin <derek.e.mauldin@gmail.com>
2015-12-16 15:50:16 -0700

    pushed messag-user-modal

--- public_html/message-user-modal.php
+++ public_html/message-user-modal.php
@@ -9,6 +9,7 @@
 
 			<!-----------------------------Modal content----------------------------------------->
 
+
 					<div class="modal-content">
 						<div class="modal-header">
 							<button type="button" class="close" data-dismiss="modal">&times;</button>
