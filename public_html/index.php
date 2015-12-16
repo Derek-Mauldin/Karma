02bb3ce9634295a8ae05541626017fddd722a71d
Jennifer Hung <jennifersukay505@gmail.com>
2015-12-16 02:07:25 -0700

    adjusted login

--- public_html/index.php
+++ public_html/index.php
@@ -87,42 +87,7 @@ require_once("php/template/head-utils.php");
 										<div id="loginError" name="loginError"></div>
 									</form><!-- #login-form -->
 
-									<form id="register-form" name="register-form" action="<?php echo $PREFIX;?>php/controllers/register-controller.php" method="post" role="form">
-										<div class="form-group">
-											<input type="text" name="firstName" id="firstName" tabindex="1" class="form-control" placeholder="First Name" value="">
-										</div><!--/form-group-->
 
-										<div class="form-group">
-											<input type="text" name="lastName" id="lastName" tabindex="2" class="form-control" placeholder="Last Name" value="">
-										</div><!--/form-group-->
-
-										<div class="form-group">
-											<input type="text" name="userName" id="userName" tabindex="3" class="form-control" placeholder="Please Choose a User Name" value="">
-										</div><!--/form-group-->
-
-										<div class="form-group">
-											<input type="email" name="email" id="email" tabindex="4" class="form-control" placeholder="Email Address" value="">
-										</div><!--/form-gorup-->
-
-										<div class="form-group">
-											<input type="password" name="password" id="password" tabindex="5" class="form-control" placeholder="Password">
-										</div><!--/form-group-->
-
-										<div class="form-group">
-											<input type="password" name="confirmPassword" id="confirmPassword" tabindex="6" class="form-control" placeholder="Confirm Password">
-										</div><!--/form-group-->
-
-										<div class="form-group">
-											<div class="row">
-												<div class="col-sm-6 col-sm-offset-3">
-													<input type="submit" name="register-submit" id="register-submit" tabindex="7" class="form-control btn btn-register" value="Register Now">
-												</div><!--/col-md-6-->
-											</div><!--/row-->
-										</div><!--/form-group-->
-										<div id="registerError" name="registerError"></div>
-									</form><!--#register-form-->
-
-								</div><!--/col-1g-12-->
 							</div><!--/row-->
 						</div><!--/panel-body-->
 					</div><!--/panel-login-->
@@ -174,9 +139,7 @@ require_once("php/template/head-utils.php");
 						<div class="panel panel-login">
 							<div class="panel-heading">
 								<div class="row">
-									<!--<div class="col-xs-6">
-										<a href="#login-form" class="active" id="login-form-link">Login</a>
-									</div>-->
+
 									<form id="login-form" name="login-form" action="php/controllers/login-controller.php" method="post" role="form">
 
 										<div class="col-xs-6 text-center" >
